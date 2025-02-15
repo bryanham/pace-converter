d41a31d2b90f740ae57e3be1b5d02f9d35509bc6
Bryan Hamilton <bryanhamilton14@gmail.com>
2025-02-15 11:29:39 +0000

    updates

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 function convert(measure) {
-    if (measure === 'km'){
-        const kmToMiles = 1.60934;
-        const mins = parseInt(document.getElementById("km-mins").value);
-        const secs = parseInt(document.getElementById("km-secs").value);
+    const conversion = 1.60934;
+    const mins = document.getElementById(measure + "-mins").valueAsNumber;
+    const secs = document.getElementById(measure + "-secs").valueAsNumber;
 
-        const totalMinutesPerKm = mins + (secs / 60);
-        
-        const pacePerMile = totalMinutesPerKm * kmToMiles;
-        const minutes = Math.floor(pacePerMile);
-        const seconds = Math.round((pacePerMile - minutes) * 60)
+    const totalMinutes = mins + (secs / 60);
+    
+    const pace = measure == 'km' ? totalMinutes * conversion : totalMinutes / conversion;
+    const minutes = Math.floor(pace);
+    const seconds = Math.round((pace - minutes) * 60);
 
+    if(measure === 'km') {
         document.getElementById('miles-mins').value = minutes;
         document.getElementById('miles-secs').value = seconds;
     } else {
-        const milesToKm = 1.60934;
-        const mins = parseInt(document.getElementById("miles-mins").value);
-        const secs = parseInt(document.getElementById("miles-secs").value);
-
-        const totalMinutesPerMile = mins + (secs / 60);
-        
-        const pacePerKm = totalMinutesPerMile / milesToKm;
-
-        const minutes = Math.floor(pacePerKm);
-        const seconds = Math.round((pacePerKm - minutes) * 60)
-
         document.getElementById('km-mins').value = minutes;
-        document.getElementById('km-secs').value = seconds;
+        document.getElementById('km-secs').value = seconds; 
     }
+    calculateRaceTimes();
+}
+
+function calculateRaceTimes() {
+    const mins = document.getElementById("km-mins").valueAsNumber;
+    const secs = document.getElementById("km-secs").valueAsNumber;
+    const fiveKmMinutes = (mins + (secs / 60)) * 5;
+    const tenKmMinutes = (mins + (secs / 60)) * 10;
+    const halfMarathonMinutes = (mins + (secs / 60)) * 21.097494;
+    const marathonMinutes = (mins + (secs / 60)) * 42.194988;
+
+    document.getElementById("five-km").innerHTML = '<p>' + fiveKmMinutes + '</p>'
 }
